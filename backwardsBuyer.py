94cba3cd8148d9da54e0d2540b4cf38821179649
Rob Clayton <rob.clayton@alintaenergy.com.au>
2022-10-17 13:54:09 +1100

    Cleanup

--- backwardsBuyer.py
+++ backwardsBuyer.py
@@ -1,20 +1,19 @@
 import pandas as pd
 
-#dataFile = "data_3600.csv"
-dataFile = "data_test.csv"
+dataFile = "data_3600.csv"
+#dataFile = "data_test.csv"
 
 # Retrieve csv data
 print("Reading data file ...")
 csvData = pd.read_csv("data/" + dataFile, sep=',')
 stock_price = list(csvData.values[:, 1])
 
-
-# min_hold = 30 + 1  # min hold time
-# max_hold = 60 + 1  # max hold time
+min_hold = 30 + 1  # min hold time
+max_hold = 60 + 1  # max hold time
 min_empty = 1  # min time step that you can be not holding the stock
 
-min_hold = 2 + 1  # min hold time
-max_hold = 4 + 1  # max hold time
+# min_hold = 2 + 1  # min hold time
+# max_hold = 4 + 1  # max hold time
 
 # start at the end of the list, and build up the optimal thing to do at each time step
 
@@ -32,14 +31,7 @@ max_profit_from_index = [0] * len(stock_price)
 
 def processStocks():
     # start at the end of the stock price list and work backwards
-    for i in range(len(stock_price)-1, -1, -1):
-
-        # Skip all the lines where we can't fit in a trade TODO: move out of the loop ???
-        if i + min_hold > len(stock_price) - 1:
-            next_index[i] = i + 1
-            best_action[i] = False
-            continue
-
+    for i in range(len(stock_price)-1 - min_hold, -1, -1):
         # Need to work out the highest total profit of all the points from i to i+max_hold
         # The points from i to min_hold won't have any additional trades
         # The points from min_hold to max_hold will have 1 additional trade
@@ -49,7 +41,8 @@ def processStocks():
         for j in range(0, min_hold):
             total_profits[j] = max_profit_from_index[i+j+1]
 
-        # set values if traded at all possible times from min hold time to max hold time + best choice in next time iteration
+        # set values if traded stock at position i at all possible times from min hold time to max hold time + best choice in next time iteration
+        # to account for total profit at given time step for given sell time
         for j in range(min_hold, min(max_hold+1, len(stock_price)-i)):
             total_profits[j] = stock_price[i+j] - stock_price[i]
             if i+j+1 < len(max_profit_from_index):
@@ -59,11 +52,14 @@ def processStocks():
         # there may be more than one max TODO: check if there are ? Does it matter?  Will hold all best, but just use the first for now
         max_index = [k for k, j in enumerate(total_profits) if j == m]
 
+        # set path taken so far for maximum result
         next_index[i] = i + max_index[0] + 1
+        # set maximum profit
         max_profit_from_index[i] = m
 
         # TODO: maybe we don't need the best action, we can read it off from the where the next index is, but whatever, lets have it for now
-        best_action[i] = max_index[0] > (min_hold - 1)
+        # I like having the direct path accessible
+        best_action[i] = max_index[0]
 
         # best path, output backwards, of course
         print(
